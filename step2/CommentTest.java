1caec06c3840ef7c3b696cb2131b27acbc8c1863
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-27 17:57:44 +0900

    Create CommentTest.java

---
+++ step2/CommentTest.java
@@ -0,0 +1,21 @@
+public class CommentTest {
+
+	public static void main(String[] args) {
+		int myInt = 10;
+		System.out.println(myInt);
+		// System.out.println(myInt);
+		
+		/*
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		System.out.println(myInt);
+		*/
+	}
+
+}
