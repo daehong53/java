f0d612a33e146b2236e9c65b804ca16656e61cd5
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-27 17:58:25 +0900

    Create ForTest02.java

---
+++ step2/ForTest02.java
@@ -0,0 +1,18 @@
+public class ForTest02 {
+
+	public static void main(String[] args) {
+		
+		/*
+		for(int i=1 ; i < 10 ; i--){
+			System.out.println("안끝난다아아아아아아!");
+		}
+		*/
+		
+		int num = 3;
+		while(num < 10){
+			System.out.println("이건 진짜 안 끝나다아아아아아아!");
+		}
+
+	}
+
+}
