599cd777646694d4b5115991c9524a5e7deca13c
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-28 21:33:34 +0900

    Create OpTest05.java

---
+++ step2/OpTest05.java
@@ -0,0 +1,11 @@
+public class OpTest05 {
+
+	public static void main(String[] args) {
+		int num = -10;
+		System.out.println( (num > 0) ? num : -num);
+		
+		int num2 = 3;
+		num2 += 10; // num2 = num2 + 10;
+		System.out.println(num2);
+	}
+}
