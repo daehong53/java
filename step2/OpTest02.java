09c26a9d1754a4fd929d14a541e0574102019c06
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-28 21:32:43 +0900

    Create OpTest02.java

---
+++ step2/OpTest02.java
@@ -0,0 +1,24 @@
+public class OpTest02 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 3;
+		
+		System.out.println("num1의 값 : " + num1);
+		System.out.println("num2의 값 : " + num2);
+		
+		System.out.println(num1 + num2); // 더하기
+		System.out.println(num1 - num2); // 빼기
+		System.out.println(num1 * num2); // 곱하기
+		System.out.println(num1 / num2); // 나누기
+		System.out.println(num1 % num2); // 나머지 연산
+		
+		double dnum1 = 3.14;
+		double dnum2 = 5.26;
+		System.out.println(dnum1 % dnum2);
+				
+				
+		
+	}
+
+}
