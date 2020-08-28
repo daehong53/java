06ecac8aadf8483f5481bf9004853195d2702c05
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-28 21:32:18 +0900

    Create OpTest01.java

---
+++ step2/OpTest01.java
@@ -0,0 +1,21 @@
+public class OpTest01 {
+
+	public static void main(String[] args) {
+		
+		int num = 3;
+		System.out.println(num++); // 3
+		System.out.println(num	); // 4
+		System.out.println(++num); // 5
+		System.out.println(num	); // 5
+		
+		int num2 = 3;
+		System.out.println(num2--);
+		System.out.println(num2);
+		
+		boolean myBool = true;
+		System.out.println(myBool);
+		System.out.println(!myBool);
+
+	}
+
+}
