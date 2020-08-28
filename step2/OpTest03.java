dd3b11078bacd1117bd32e371984416ea69045f2
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-28 21:33:02 +0900

    Create OpTest03.java

---
+++ step2/OpTest03.java
@@ -0,0 +1,21 @@
+public class OpTest03 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 3;
+		
+		System.out.println(num1 >  num2);
+		System.out.println(num1 <  num2);
+		System.out.println(num1 >= num2);
+		System.out.println(num1 <= num2);
+		System.out.println(num1 == num2);
+		System.out.println(num1 != num2);
+		
+		boolean myBool = num1 > num2;
+		
+		
+		
+
+	}
+
+}
