c3c3982ec0da9b31c8c1d8623c8574a1fb9929a4
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-29 14:51:52 +0900

    Create ArrayTest03.java

---
+++ step3/ArrayTest03.java
@@ -0,0 +1,24 @@
+public class ArrayTest03 {
+
+	public static void main(String[] args) {
+		
+		int[][] score = new int[3][4];
+		
+		score[0][0] = 100;
+		score[0][1] = 99;
+		score[0][2] = 98;
+		score[0][3] = 97;
+		score[1][0] = 39;
+		score[1][1] = 38;
+		score[1][2] = 37;
+		score[1][3] = 36;
+		score[2][0] = 19;
+		score[2][1] = 18;
+		score[2][2] = 17;
+		score[2][3] = 16;
+		
+		
+		
+	}
+
+}
