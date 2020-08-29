9a4e756da8a8e6b841224b60de727fb348ba71e4
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-29 14:52:38 +0900

    Create BreakTest01.java

---
+++ step3/BreakTest01.java
@@ -0,0 +1,13 @@
+public class BreakTest01 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1 ; i<=10 ; i++){
+			if(i==5) break;
+			System.out.println("몇 번이나 실행될까?");
+		}
+		System.out.println("실행끝");
+		
+	}
+
+}
