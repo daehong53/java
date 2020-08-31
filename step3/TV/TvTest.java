d0aef0edbfef723aec2414db00df0cb691ae0391
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-08-31 16:57:19 +0900

    Create TvTest.java

---
+++ step3/TV/TvTest.java
@@ -0,0 +1,26 @@
+public class TvTest {
+
+	public static void main(String[] args) {
+		Tv myTv = new Tv();
+		
+		myTv.power = true;
+		myTv.channel = 7;
+		myTv.color = "Black";
+		
+		myTv.power();
+		
+//		Tv momTv = new Tv();
+//		Tv dadTv = new Tv();
+//		Tv sisTv = new Tv();
+		
+		Tv momTv = myTv;
+		
+		int num1 = 10;
+		int num2 = num1;
+		
+		
+		
+		
+	}
+
+}
