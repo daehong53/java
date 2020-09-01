61212f0b2d89f41204974493b0e1882950ef6847
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-01 16:53:07 +0900

    Create MethodOverload.java

---
+++ step4/Method/MethodOverload.java
@@ -0,0 +1,15 @@
+public class MethodOverload {
+	int plus(int a, int b){
+		return a+b;
+	}
+	/*int plus(int c, int d){
+		return c+d;
+	}*/
+	int plus(int a, int b, int c){
+		return a+b+c;
+	}
+	double plus(double a, double b){
+		return a+b;
+	}
+
+}
