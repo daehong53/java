c63055d71de20484fe65c67f3d2c1a11f134ff0c
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-02 17:37:11 +0900

    Create StringTest02.java

---
+++ step4/String/StringTest02.java
@@ -0,0 +1,17 @@
+public class StringTest02 {
+
+	public static void main(String[] args) {
+		
+		// "철수야 안녕?" 라고 영희가 말했다.
+		// escape char
+		String str = "\"철수야 안녕?\" 라고 영희가 말했다.";
+		System.out.println(str);
+		
+		// "그건 C:\Program Files\Java 안에 있어" 라고 철수가 말했다.
+		String str2 = "\"그건 C:\\Program Files\\Java 안에 있어\" 라고 철수가 말했다.";
+		System.out.println(str2);
+		
+
+	}
+
+}
