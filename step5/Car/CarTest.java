4c253463959671fd49d3d7cb385a5dc5bf5c5466
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-03 15:45:52 +0900

    Create CarTest.java

---
+++ step5/Car/CarTest.java
@@ -0,0 +1,11 @@
+public class CarTest {
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		Car c2 = new Car("Blue");
+		Car c3 = new Car("Red", "stick", 2);
+		
+		System.out.println(c1.color +":"+ c1.gearType +":"+ c1.door);
+		System.out.println(c2.color +":"+ c2.gearType +":"+ c2.door);
+		System.out.println(c3.color +":"+ c3.gearType +":"+ c3.door);
+	}
+}
