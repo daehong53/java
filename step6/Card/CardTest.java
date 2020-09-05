3f121e8e4c2a1e624dd3f62f5be27f3449e7080c
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-05 14:36:30 +0900

    Create CardTest.java

---
+++ step6/Card/CardTest.java
@@ -0,0 +1,12 @@
+import myPkg.Card;
+
+public class CardTest {
+
+	public static void main(String[] args) {
+		Card myCard = new Card();
+		
+		myCard.printCardNumber();
+//		myCard.printCardKind(); // 요거는 못씀
+	}
+
+}
