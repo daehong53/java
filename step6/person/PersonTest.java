10da95a98a16dfc1aae696bcf0f4cb7dba88add8
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-08 22:37:31 +0900

    Create PersonTest.java

---
+++ step6/person/PersonTest.java
@@ -0,0 +1,14 @@
+public class PersonTest {
+
+	public static void main(String[] args) {
+		Person brother = new Person();
+		brother.age = 22;
+		brother.height = 170.0;
+//		brother.weight = 67.0;
+		
+		System.out.println("age : " + brother.age);
+		System.out.println("height : " + brother.height);
+//		System.out.println("weight : " + brother.weight);
+	}
+
+}
