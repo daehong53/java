71c62743c8b6a5b3dc0095a9f28825e3163857d2
daehong53 <70262381+daehong53@users.noreply.github.com>
2020-09-11 11:33:41 +0900

    Create Dvd.java

---
+++ step7/Dvd.java
@@ -0,0 +1,26 @@
+public class Dvd {
+	
+	int counter;
+	
+	public void play(){
+		// ... 
+	}
+	
+	public void stop(){
+		// ...
+	}
+	
+	public void reset(){
+		// counter = 0;
+	}
+	
+	public int getCounter(){
+		// ...
+		return counter;
+	}
+	
+	public void setCounter(int c){
+		counter = c;
+	}
+
+}
