8ece8e5f26aff85e8698e4b0a7e3b3f6b9298373
Daehong Kim <70262381+daehong53@users.noreply.github.com>
2020-09-22 20:36:15 +0900

    Create ExceptionEx3.java

---
+++ step8/ExceptionEx3.java
@@ -0,0 +1,13 @@
+public class ExceptionEx3 {
+
+	public static void main(String[] args) {
+		
+		int[] num = new int[3];
+		
+		for(int i=0 ; i<=3 ; i++){
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
